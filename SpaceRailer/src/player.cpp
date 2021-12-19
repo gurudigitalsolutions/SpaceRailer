891e4b7b913d9ef9d828883044f5d9bd3fc29c8e
Brian Murphy <gurudvlp@gmail.com>
2021-12-19 00:26:38 -0500

    Added boundaries so that the player is unable to go off screen.

--- SpaceRailer/src/player.cpp
+++ SpaceRailer/src/player.cpp
@@ -1,8 +1,20 @@
 #include "player.h"
 
+extern short windowWidth;
+extern short windowHeight;
 
 bool Player::initialize()
 {
 	return Mob::initialize("playership");
 }
 
+bool Player::process()
+{
+	if(getX() < 0) { setX(0); }
+	if(getY() < 0) { setY(0); }
+	
+	if(getX() > windowWidth - 1 - getWidth()) { setX(windowWidth - 1 - getWidth()); }
+	if(getY() > windowHeight - 1 - getHeight()) { setY(windowHeight - 1 - getHeight()); }
+	
+	return Mob::process();
+}
--- SpaceRailer/src/player.h
+++ SpaceRailer/src/player.h
@@ -5,4 +5,5 @@ private:
 
 public:
 	bool initialize();
+	bool process();
 };
